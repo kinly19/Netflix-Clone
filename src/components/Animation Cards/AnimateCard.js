ee3dde02a0de98fd0c76b31a9b2bdce928a50cf0
unknown <kinchoily@hotmail.com>
2022-05-18 18:04:29 +0100

    AnimateCard.js - Update component, add class styles for additional wrapper container

--- src/components/Animation Cards/AnimateCard.js
+++ src/components/Animation Cards/AnimateCard.js
@@ -1,26 +1,31 @@
 import "./AnimateCard.scss";
 
 const AnimateCard = (props) => {
-  let containerClass = props.reverseRow
+  const contentClass = props.reverseRow
     ? "animate-card__content animate-card__content--reverse"
     : "animate-card__content";
-  let textClass = props.reverseRow
+  const textClass = props.reverseRow
     ? "animate-card__text animate-card__text--padRight"
     : "animate-card__text";
-  let animateContainerClass = props.modifierClass
-    ? `animate-card__animation ${props.modifierClass}`
-    : "animate-card__animation";
-
+  const wrapperClass = props.wrapperModClass
+    ? `animate-card__animation-container animate-card__animation-container--${props.wrapperModClass}`
+    : "animate-card__animation-container";
+  const animateContentClass = props.animateModClass
+    ? `animate-card__animation animate-card__animation--${props.animateModClass}`
+    : `animate-card__animation`;
+  
   return (
     <div className="animate-card">
-      <div className={containerClass}>
+      <div className={contentClass}>
         <div className={textClass}>
           <h1>{props.title}</h1>
           <h2>{props.text}</h2>
         </div>
         <div className="animate-card__image-container">
-          <img className="animate-card__img" src={props.image} />
-          <div className={animateContainerClass}>{props.children}</div>
+          <div className={wrapperClass}>
+            <img className="animate-card__img" src={props.image} />
+            <div className={animateContentClass}>{props.children}</div>
+          </div>
         </div>
       </div>
     </div>
