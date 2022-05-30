52f2b4f2b43afa208f4131bd90ae4a05b61e5b47
unknown <kinchoily@hotmail.com>
2022-05-30 17:55:24 +0100

    NavSecondary.js - Update component, add conditional class styles

--- src/components/Navbar/NavSecondary.js
+++ src/components/Navbar/NavSecondary.js
@@ -2,13 +2,24 @@ import "./NavSecondary.scss";
 import { Link } from 'react-router-dom';
 import { ReactComponent as NetflixLogo } from '../../assets/Images/Netflix.svg';
 
-const NavSecondary = () => {
+const NavSecondary = (props) => {
+      
+  let navClass = "navSec"
+  let logoClass = "navSec__logo"
+  let linkClass = "navSec__link"    
+
+  if (props.navType === "light") {
+    navClass = "navSec navSec--borderBtm"
+    logoClass = "navSec__logo navSec__logo--small"
+    linkClass = "navSec__link navSec__link--light"
+  }
+  
   return (
-    <nav className="navSec">
-      <div className="navSec__logo">
+    <nav className={navClass}>
+      <div className={logoClass}>
         <NetflixLogo />
       </div>
-      <Link className="navSec__link" to={"login"}>
+      <Link className={linkClass} to={"login"}>
         Sign in
       </Link>
     </nav>
